e65578308ab6320181dc42df90380b031bd5e6f6
Kelly Ruiz <kellyruiz.dev@gmail.com>
2026-05-26 18:02:11 -0400

    Initial Commit

---
+++ TraderUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TraderUI
+//
+//  Created by Kelly Ruiz on 26/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
